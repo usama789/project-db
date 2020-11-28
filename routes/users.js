82215291f80ed74d0d8538c9400a998289d8989a
usama789 <>
2020-11-28 18:56:19 +0500

    db whole

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var User =require('../Model/users');
+/* GET users listing. */
+router.get('/register', function(req, res, next) {
+  res.render('users/register');
+});
+router.post('/register', async function(req, res, next) {
+    var user = new User(req.body);
+    await user.save();
+    res.redirect('/');
+});
+
+module.exports = router;
